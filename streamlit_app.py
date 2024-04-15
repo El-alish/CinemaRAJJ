9ed3fd3d5f41fe49e42ae029e7719564fe451840
El-alish <144781060+El-alish@users.noreply.github.com>
2024-04-15 15:00:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,12 @@ st.set_page_config(
     page_icon="⭐",
 )
 
+with st.sidebar:
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(f"Echo: {prompt}")
+
 st.title("Bienvenue au cinéma RAJJ - Élu meilleur cinéma de la Creuse")
 
 st.image('https://t4.ftcdn.net/jpg/04/46/93/93/360_F_446939375_83iP0UYTg5F9vHl6icZwgrEBHXeXMVaU.jpg', caption='Vous ne savez pas quoi regarder ?')
