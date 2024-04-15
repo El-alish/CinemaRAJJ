d01bad38ceff2f818078b45b6361a83f945ad708
El-alish <144781060+El-alish@users.noreply.github.com>
2024-04-15 09:45:54 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -8,13 +8,13 @@ from streamlit_option_menu import option_menu
 
 
 st.set_page_config(
-    page_title="Multipage App",
-    page_icon="👋",
+    page_title="RAJJ - Cinema",
+    page_icon="⭐",
 )
 
 st.title("Bienvenue au cinéma RAJJ - Élu meilleur cinéma de la Creuse")
 
 st.image('https://t4.ftcdn.net/jpg/04/46/93/93/360_F_446939375_83iP0UYTg5F9vHl6icZwgrEBHXeXMVaU.jpg', caption='Vous ne savez pas quoi regarder ?')
 
-st.write("Merci d'utiliser notre menu de gauche pour accéder aux diverses pages de notre site, merci")
-st.sidebar.success("Menu.")
+st.write("Merci d'utiliser notre menu de gauche pour accéder aux diverses pages de notre site.")
+st.sidebar.success("Faites votre choix pour naviguer sur le site. ☝️ ")
