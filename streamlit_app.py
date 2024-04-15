9c9ec6617922a6e60746c705abd7dafd7037c45d
El-alish <144781060+El-alish@users.noreply.github.com>
2024-04-15 09:48:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,20 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+from streamlit_navigation_bar import st_navbar
+from streamlit_option_menu import option_menu
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 st.set_page_config(
-    page_title="Cinema RAJJ",
-    page_icon="👋",
+    page_title="RAJJ - Cinema",
+    page_icon="⭐",
 )
 
 st.title("Bienvenue au cinéma RAJJ - Élu meilleur cinéma de la Creuse")
 
 st.image('https://t4.ftcdn.net/jpg/04/46/93/93/360_F_446939375_83iP0UYTg5F9vHl6icZwgrEBHXeXMVaU.jpg', caption='Vous ne savez pas quoi regarder ?')
 
-st.write("Merci d'utiliser notre menu de gauche pour accéder aux diverses pages de notre site, merci")
-st.sidebar.success("Menu.")
+st.write("Merci d'utiliser notre menu de gauche pour accéder aux diverses pages de notre site.")
+st.sidebar.success("Faites votre choix pour naviguer sur le site. ☝️ ")
