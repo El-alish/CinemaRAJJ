5a05bf726aa8818c56f0dc727292f0c45c39731b
El-alish <144781060+El-alish@users.noreply.github.com>
2024-04-15 08:51:33 +0200

    Update streamlit_app.py
    Test MAJ directement dans la page

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,14 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+st.title("Bienvenue au cinéma RAJJ - Élu meilleur cinéma de la Creuse")
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+st.image('https://t4.ftcdn.net/jpg/04/46/93/93/360_F_446939375_83iP0UYTg5F9vHl6icZwgrEBHXeXMVaU.jpg', caption='Vous ne savez pas quoi regarder ?')
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.write("Merci d'utiliser notre menu de gauche pour accéder aux diverses pages de notre site, merci")
+st.sidebar.success("Menu.")
