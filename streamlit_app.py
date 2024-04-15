58d477dc5a08d0bf21ab8a630cab7276413e06d2
El-alish <144781060+El-alish@users.noreply.github.com>
2024-04-15 11:00:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
 
 st.set_page_config(
     page_title="RAJJ - Cinema",
